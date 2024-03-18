dd4681ca55e40e7ba80b6bc994c1555e90fd0173
Nalson <gahgah147@gmail.com>
2024-03-18 17:35:57 +0800

    ep2

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	. "golangAPI/src"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "ping",
-		})
-	})
-	router.POST("/ping/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		c.JSON(200, gin.H{
-			"id": id,
-		})
-	})
+	v1 := router.Group("/v1")
+	AddUserRouter(v1)
+
+	// router.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "ping",
+	// 	})
+	// })
+	// router.POST("/ping/:id", func(c *gin.Context) {
+	// 	id := c.Param("id")
+	// 	c.JSON(200, gin.H{
+	// 		"id": id,
+	// 	})
+	// })
 
 	router.Run(":8000")
 }
