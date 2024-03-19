f419ecfd7377b29414d7a53786872ece058ee6c6
Nalson <gahgah147@gmail.com>
2024-03-19 13:48:12 +0800

    nalson 2024-03-19 EP3

--- service/UserService.go
+++ service/UserService.go
@@ -2,7 +2,9 @@ package service
 
 import (
 	"golangAPI/pojo"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,4 +27,33 @@ func PostUser(c *gin.Context) {
 	}
 	userList = append(userList, user)
 	c.JSON(http.StatusOK, "Successfully posted")
+}
+
+// delete User
+func DeleteUser(c *gin.Context) {
+	userId, _ := strconv.Atoi(c.Param("id"))
+	for _, user := range userList {
+		log.Println(user)
+		userList = append(userList[:userId], userList[userId+1:]...)
+		c.JSON(http.StatusOK, "Successfully deleted")
+		return
+	}
+	c.JSON(http.StatusNotFound, "Error")
+}
+
+func PutUser(c *gin.Context) {
+	beforeUser := pojo.User{}
+	err := c.BindJSON(&beforeUser)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "Error")
+	}
+	userId, _ := strconv.Atoi(c.Param("id"))
+	for key, user := range userList {
+		if userId == user.Id {
+			userList[key] = beforeUser
+			log.Println(userList[key])
+			c.JSON(http.StatusOK, "Successfully")
+		}
+	}
+	c.JSON(http.StatusNotFound, "Error")
 }
--- src/UserRouter.go
+++ src/UserRouter.go
@@ -10,4 +10,10 @@ func AddUserRouter(r *gin.RouterGroup) {
 	user := r.Group("/users")
 	user.GET("/", service.FindAllUsers)
 	user.POST("/", service.PostUser)
+	// delete user
+	user.DELETE("/:id", service.DeleteUser)
+	// put user
+	user.PUT("/:id", service.PutUser)
 }
+
+// put User
